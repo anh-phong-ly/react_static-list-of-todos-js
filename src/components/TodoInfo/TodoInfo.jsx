f19e79d77476a00903ffcd859422d496dfe2698d
PhongLy <ly.phong165@gmail.com>
2025-01-29 22:03:45 +0700

    fix test

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -7,7 +7,7 @@ import { UserInfo } from '../UserInfo';
 export const TodoInfo = ({ todo }) => {
   return (
     <article
-      className={`TodoInfo${todo.completed ? 'TodoInfo--completed' : ''}`}
+      className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
     >
       <h2 className="TodoInfo__title">{todo.title}</h2>
       {todo.user && <UserInfo user={todo.user} />}
